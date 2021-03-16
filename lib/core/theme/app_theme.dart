c789c4a623b3434959396fa2b6ba9cc9606d6114
adaojreess <adaojreess@gmail.com>
2021-03-16 09:23:19 -0300

    initial

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+import 'app_colors.dart';
+
+class AppTheme {
+  static ThemeData get theme => ThemeData(
+        fontFamily: 'Nunito-Sans',
+        primarySwatch: AppColors.primaryColor,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      );
+}
---
+++ lib/views/content/content_page.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:ifood_flutter_clone/core/theme/app_colors.dart';
+import 'package:ifood_flutter_clone/core/theme/app_typography.dart';
+import 'package:tab_indicator_styler/tab_indicator_styler.dart';
+
+import 'components/header_location_component.dart';
+
+class ContentPage extends StatefulWidget {
+  @override
+  _ContentPageState createState() => _ContentPageState();
+}
+
+class _ContentPageState extends State<ContentPage>
+    with SingleTickerProviderStateMixin {
+  TabController tabController;
+
+  @override
+  void initState() {
+    tabController = TabController(length: 2, vsync: this);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          HeaderLocationComponent(location: 'Rua das Flores do Campo, 10'),
+          Padding(
+            padding: const EdgeInsets.only(left: 16, right: 170),
+            child: TabBar(
+              labelPadding: EdgeInsets.zero,
+              indicatorPadding: EdgeInsets.zero,
+              indicator: MaterialIndicator(
+                color: AppColors.primaryColor,
+                height: 2,
+                bottomLeftRadius: 5,
+                bottomRightRadius: 5,
+              ),
+              controller: tabController,
+              labelColor: AppColors.black,
+              unselectedLabelColor: AppColors.black54,
+              labelStyle: AppTypography.bodyText(context).copyWith(
+                color: Colors.black,
+              ),
+              tabs: [
+                Tab(
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text('Restaurantes'),
+                  ),
+                ),
+                Tab(
+                  child: Text('Mercados'),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
